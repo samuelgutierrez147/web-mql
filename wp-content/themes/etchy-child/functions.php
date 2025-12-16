ad8cc31ba85bd2e9051192362cbb6f96a2986976
Samuel <soporte@masquelibrosdigital.com>
2025-12-16 12:20:35 +0100

    CAMBIOS WEB ponemos pruebas en bd (TEST)

--- wp-content/themes/etchy-child/functions.php
+++ wp-content/themes/etchy-child/functions.php
@@ -967,7 +967,7 @@ add_action('woocommerce_before_single_product', 'redirect_non_logged_users_to_lo
 function getConfigUrlOptimus($optimusUri)
 {
     $urlBase = "http://81.42.209.224:8080/optwebsvcs/";
-    $configDbOptimus = "masquelibros";
+    $configDbOptimus = "pruebas";
     return $urlBase . $optimusUri . '?db=' . $configDbOptimus;
 }
 
