bd2c9c568776cdfc36abf7c5e90f65e4b34ce67e
Samuel <soporte@masquelibrosdigital.com>
2026-01-09 10:26:04 +0100

    CAMBIOS WEB añadimos soporte para slider producto

--- wp-content/themes/etchy-child/functions.php
+++ wp-content/themes/etchy-child/functions.php
@@ -14,6 +14,12 @@ if (!function_exists('etchy_child_theme_enqueue_scripts')) {
     add_action('wp_enqueue_scripts', 'etchy_child_theme_enqueue_scripts');
 }
 
+add_action('after_setup_theme', function () {
+    add_theme_support('wc-product-gallery-zoom');
+    add_theme_support('wc-product-gallery-lightbox');
+    add_theme_support('wc-product-gallery-slider');
+});
+
 // Incluir Bootstrap CSS
 function bootstrap_css()
 {
